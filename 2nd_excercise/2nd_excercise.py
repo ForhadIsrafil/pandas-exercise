2dc3f9cc4c8e39bc2827380cdce5d2f05f13a7a0
forhadisrafil <forhadtuhin5088@gmail.com>
2020-08-25 03:11:47 +0600

    2nd excercise wip

--- 2nd_excercise/2nd_excercise.py
+++ 2nd_excercise/2nd_excercise.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import numpy as np
-import os
+import os, sys
 import xlsxwriter
 from openpyxl import load_workbook
 
-
+# newfile=os.path.join(pth,'combined.xlsx') # Todo: save file any other directory
+file = os.path.join('../demo_sheets/SampleData.xlsx')
+data = pd.read_excel(file)
+df = pd.DataFrame(data)
+# print(df[(df['Units'] == 27) & (df['Rep'] == 'Gill')])
+print(df[df['Units'] > 80])
