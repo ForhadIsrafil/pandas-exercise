dbc5ab28b2a7a22e49f3d9f29a38766b34fd5728
forhadisrafil <forhadtuhin5088@gmail.com>
2020-09-03 00:51:30 +0600

    new pandas excel files and codes

--- github_api_data_to_csv.py
+++ github_api_data_to_csv.py
@@ -3,10 +3,18 @@ import json
 import requests
 import pandas as pd
 
-url = "https://api.github.com/repositories?since=364"
+# url = "https://api.github.com/repositories?since=364"
+#
+# data = requests.get(url)
+# # print(json.loads(data.content))
+# response_data = json.loads(data.content)
+# d_data = pd.DataFrame(response_data)
+# d_data.to_csv('res_pata.csv', index=False)
 
-data = requests.get(url)
-# print(json.loads(data.content))
-response_data = json.loads(data.content)
-d_data = pd.DataFrame(response_data)
-d_data.to_csv('res_pata.csv', index=False)
+# get organizations repositories
+url = "https://api.github.com/users/octocat/repos"
+header = {
+    'Accept': 'application/vnd.github.baptiste-preview+json'
+}
+data = requests.get(url, headers=header)
+print(data.content)
